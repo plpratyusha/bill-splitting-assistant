5fcf205098cfbec9713a3f4423a117b27eedf990
Pratyusha Pogaru <plpratyusha@gmail.com>
2018-10-23 19:45:47 -0700

    First commit

---
+++ app/src/main/java/com/example/pratyushapogaru/billsplittingassistant/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pratyushapogaru.billsplittingassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // program creates an instance of this class when build & run
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
